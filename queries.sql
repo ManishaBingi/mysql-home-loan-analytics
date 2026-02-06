07845d53fc1e9d85bb54afe71062292c6b9492e4
ManishaBingi <bingi.manisha@gmail.com>
2026-02-06 21:50:35 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -52,7 +52,7 @@ JOIN Payments p ON l.loan_id = p.loan_id
 GROUP BY l.loan_id, c.name, l.loan_amount
 HAVING repayment_percentage >= 80
 ORDER BY repayment_percentage DESC;
--- City-wise Average Income vs Loan Amount
+-- 11.City-wise Average Income vs Loan Amount
 SELECT c.city,
        ROUND(AVG(c.income),2) AS avg_income,
        ROUND(AVG(l.loan_amount),2) AS avg_loan_amount,
@@ -61,7 +61,7 @@ FROM Customers c
 JOIN Loans l ON c.customer_id = l.customer_id
 GROUP BY c.city
 ORDER BY loan_to_income_ratio DESC;
--- Interest Revenue Projection (Expected Total Interest)
+-- 12.Interest Revenue Projection (Expected Total Interest)
 SELECT l.loan_id, c.name,
        l.loan_amount,
        l.interest_rate,
@@ -69,15 +69,30 @@ SELECT l.loan_id, c.name,
 FROM Loans l
 JOIN Customers c ON l.customer_id = c.customer_id
 ORDER BY projected_interest DESC;
--- Delayed Payments (Payments after due date assumption)
+-- 13. Delayed Payments (Payments after due date assumption)
 SELECT p.payment_id, c.name, l.loan_id, p.payment_date, l.start_date
 FROM Payments p
 JOIN Loans l ON p.loan_id = l.loan_id
 JOIN Customers c ON l.customer_id = c.customer_id
 WHERE MONTH(p.payment_date) <> MONTH(l.start_date);
--- Customer Loan Tenure Distribution
+-- 14. Customer Loan Tenure Distribution
 SELECT tenure_years, COUNT(*) AS total_loans
 FROM Loans
 GROUP BY tenure_years
 ORDER BY tenure_years;
+-- 15. Default Risk Flag (Customers with High Loan-to-Income Ratio and Low Payments)
+ SELECT c.name, c.income, l.loan_amount,
+       ROUND(l.loan_amount/c.income,2) AS loan_to_income_ratio,
+       IFNULL(SUM(p.amount),0) AS total_paid,
+       l.loan_amount - IFNULL(SUM(p.amount),0) AS outstanding_balance,
+       CASE 
+         WHEN (l.loan_amount/c.income) > 10 AND (SUM(p.amount) IS NULL OR SUM(p.amount) < (0.2*l.loan_amount))
+         THEN 'High Risk'
+         ELSE 'Normal'
+       END AS risk_flag
+FROM Customers c
+JOIN Loans l ON c.customer_id = l.customer_id
+LEFT JOIN Payments p ON l.loan_id = p.loan_id
+GROUP BY c.name, c.income, l.loan_amount
+ORDER BY risk_flag DESC, outstanding_balance DESC;
 
